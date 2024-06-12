f6064b961178ca18eddc2d7688ef64fd248063ef
vitinnDev <165920404+vitinnDev@users.noreply.github.com>
2024-06-12 09:34:54 -0300

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,13 +1,6 @@
-print("Uma calculadora programada em Python")
-numberOne = float(input("Digite o primeiro número: "))
-numberTwo = float(input("Digite o segundo número: "))
-choice = input("Escolha uma operação básica: ")
-print()
-if choice == "+":
-    print(f'O valor é {numberOne + numberTwo}')
-elif choice == "-":
-    print(f'O valor é {numberOne - numberTwo}')
-elif choice == "*":
-    print(f'O valor é {numberOne * numberTwo}')
-else:
-    print(f'O valor é {numberOne / numberTwo}')
+unumero = int(input('digite o primeiro número: '))
+dnumero = int(input('digite o segundo número: '))
+print(f'A soma é igual {unumero + dnumero}')
+print(f'A subtração é igual {unumero - dnumero}')
+print(f'A multiplicação é igual {unumero * dnumero}')
+print(f'A divisão é igual {unumero / dnumero}')
