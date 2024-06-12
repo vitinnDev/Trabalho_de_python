e8af78f6da63350ca49803223fc2ad04cea540c3
vitinnDev <165920404+vitinnDev@users.noreply.github.com>
2024-06-12 09:32:57 -0300

    Update Q4.py

--- Q4.py
+++ Q4.py
@@ -1,2 +1,2 @@
-frase = input("Digite uma frase: ")
-print(f'O último caractere da frase "{frase}" é {frase[len(frase)-1]}')
+frase = input('digite uma frase: ')
+print(f'o último caractere da frase é {frase[len(frase)-1]}')
